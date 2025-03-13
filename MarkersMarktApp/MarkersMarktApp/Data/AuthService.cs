353cbfc4e10334e577f529484e7bc0ed6a28fc50
Julian Poppelaars <D295927@edu.curio.nl>
2025-03-13 11:38:50 +0100

    Benodigde bestanden toegevoegd

---
+++ MarkersMarktApp/MarkersMarktApp/Data/AuthService.cs
@@ -0,0 +1,29 @@
+﻿using MakersMarktApp.Data;
+using System.Linq;
+
+namespace MakersMarktApp.Services
+{
+    public static class AuthService
+    {
+        public static int? CurrentUserId { get; private set; }
+
+        public static User Login(string username, string password)
+        {
+            using (var db = new AppDbContext())
+            {
+                var user = db.Users.FirstOrDefault(u => u.Username == username && u.Password == password);
+                if (user != null)
+                {
+                    CurrentUserId = user.Id;
+                    return user;
+                }
+            }
+            return null;
+        }
+
+        public static void Logout()
+        {
+            CurrentUserId = null;
+        }
+    }
+}
