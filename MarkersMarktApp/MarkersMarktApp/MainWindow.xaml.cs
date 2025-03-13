f391b3378f8929bd317c1be77e35a8171954541e
Paper <rubenschalk08@gmail.com>
2025-03-13 09:34:11 +0100

    Made the login and register windows

--- MarkersMarktApp/MarkersMarktApp/MainWindow.xaml.cs
+++ MarkersMarktApp/MarkersMarktApp/MainWindow.xaml.cs
@@ -1,3 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using MarkersMarktApp.Data;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -5,13 +11,9 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using static System.Net.Mime.MediaTypeNames;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -23,14 +25,78 @@ namespace MarkersMarktApp
     /// </summary>
     public sealed partial class MainWindow : Window
     {
-        public MainWindow()
+        public MainWindow(bool isCreated)
         {
             this.InitializeComponent();
+
+            if(!isCreated)
+            {
+                using (var db = new AppDbContext())
+                {
+                    db.Database.EnsureDeleted();
+                    db.Database.EnsureCreated();
+                }
+            }
+
+            if (isCreated == true)
+            {
+                accountCreated.Visibility = Visibility.Visible;
+            }
         }
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
+        private void Login_Click(object sender, RoutedEventArgs e)
         {
-            myButton.Content = "Clicked";
+            var mail = emailTextBox.Text;
+            var password = passPasswordBox.Password;
+            if ( mail.Count() == 0 && password.Count() == 0 )
+            {
+                fillError.Text = "Fill in every field!";
+                fillError.Visibility = Visibility.Visible;
+                emailError.Visibility = Visibility.Collapsed;
+                passError.Visibility = Visibility.Collapsed;
+
+            }
+            else if (mail.Count() == 0)
+            {
+                fillError.Visibility = Visibility.Collapsed;
+                emailError.Visibility = Visibility.Visible;
+                passError.Visibility = Visibility.Collapsed;
+            }
+            else if (password.Count() == 0)
+            {
+                fillError.Visibility = Visibility.Collapsed;
+                emailError.Visibility = Visibility.Collapsed;
+                passError.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                fillError.Text = "User not found!";
+                fillError.Visibility = Visibility.Collapsed;
+                emailError.Visibility = Visibility.Collapsed;
+                passError.Visibility = Visibility.Collapsed;
+
+                using (var db = new AppDbContext())
+                {
+                    var user = db.Users.Where(u => u.Email.ToLower().Equals(mail.ToLower()) && u.Password.Equals(password)).FirstOrDefault();
+                    if (user == null)
+                    {
+                        fillError.Visibility = Visibility.Visible;
+                    }
+                    else
+                    {
+                        var itemWindow = new ItemWindow(user.Id);
+                        itemWindow.Activate();
+                        this.Close();
+                    }
+                }
+            }
+        }
+
+        private void Register_Click(object sender, RoutedEventArgs e)
+        {
+            var registerWindow = new RegisterWindow();
+            registerWindow.Activate();
+            this.Close();
         }
     }
 }
