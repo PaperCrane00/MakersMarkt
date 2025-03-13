201e94a70aa450333efa5182900d809d6b441e67
Julian Poppelaars <D295927@edu.curio.nl>
2025-03-13 11:56:14 +0100

    MainPage

--- MarkersMarktApp/MarkersMarktApp/Pages/MainPage.xaml.cs
+++ MarkersMarktApp/MarkersMarktApp/Pages/MainPage.xaml.cs
@@ -1,31 +1,74 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using MakersMarktApp.Data;
+using MakersMarktApp.Services;
+using MarkersMarktApp.Pages;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
-
-namespace MarkersMarktApp.Pages
+namespace MakersMarktApp.Pages
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
+        private List<Product> allUserProducts = new List<Product>();
+
         public MainPage()
         {
             this.InitializeComponent();
+            LoadUserProducts();
+        }
+
+        private void LoadUserProducts()
+        {
+            int? userId = AuthService.CurrentUserId;
+
+            if (userId == null)
+            {
+                NavigationService.NavigateTo(typeof(LoginPage));
+                return;
+            }
+
+            using (var db = new AppDbContext())
+            {
+                allUserProducts = db.Products
+                                    .Where(p => p.UserId == userId)
+                                    .ToList();
+            }
+        }
+
+        private void LogoutButton_Click(object sender, RoutedEventArgs e)
+        {
+            AuthService.Logout();
+            ShowMessage("U bent uitgelogd!");
+            NavigationService.NavigateTo(typeof(LoginPage));
+        }
+
+        private void ShowMessage(string message)
+        {
+            ContentDialog dialog = new ContentDialog()
+            {
+                Title = "Informatie",
+                Content = message,
+                CloseButtonText = "OK",
+                XamlRoot = this.XamlRoot
+            };
+            _ = dialog.ShowAsync();
+        }
+
+        private async void UserProductsListView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            var clickedItem = (Product)e.ClickedItem;
+
+            ContentDialog itemDetailsDialog = new ContentDialog()
+            {
+                Title = clickedItem.Name,
+                Content = $"Beschrijving: {clickedItem.Description}\n" +
+                          $"Prijs: {clickedItem.Price}\n",
+                CloseButtonText = "OK",
+                XamlRoot = this.XamlRoot
+            };
+            await itemDetailsDialog.ShowAsync();
         }
     }
 }
