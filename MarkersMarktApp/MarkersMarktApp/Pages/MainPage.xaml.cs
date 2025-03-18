13004079657248e2c0a6a33eb3b041f4b83228d3
Paper <rubenschalk08@gmail.com>
2025-03-18 10:14:50 +0100

    Solved merge conflict

--- MarkersMarktApp/MarkersMarktApp/Pages/MainPage.xaml.cs
+++ MarkersMarktApp/MarkersMarktApp/Pages/MainPage.xaml.cs
@@ -9,8 +9,6 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
 using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
@@ -22,7 +20,6 @@ namespace MakersMarktApp.Pages
     public sealed partial class MainPage : Page
     {
        private List<Product> allUserProducts = new List<Product>();
-
         public MainPage()
         {
             this.InitializeComponent();
