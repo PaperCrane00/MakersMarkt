f346a6c06d240ed6976bc40d4e3039b873d61ded
Bradjo555 <72301431+Bradjo555@users.noreply.github.com>
2025-03-18 10:30:05 +0100

    maak product.cs public p.s MAAK ALLES PUBLIC

--- MarkersMarktApp/MarkersMarktApp/Data/Product.cs
+++ MarkersMarktApp/MarkersMarktApp/Data/Product.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MakersMarktApp.Data
 {
-    internal class Product
+    public class Product
     {
         public int Id { get; set; }
         public string Name { get; set; }
