92e813c48028911ab9db6bb95bca580627a725eb
Julian Poppelaars <D295927@edu.curio.nl>
2025-03-18 12:29:43 +0100

    Beschrijving van producten beschikbaar
    Om het te zien moet je op het product klikken in de listview

--- MarkersMarktApp/MarkersMarktApp/Pages/MainPage.xaml.cs
+++ MarkersMarktApp/MarkersMarktApp/Pages/MainPage.xaml.cs
@@ -2,6 +2,7 @@ using MakersMarktApp.Data;
 using MarkersMarktApp.Pages;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -31,6 +32,21 @@ namespace MakersMarktApp.Pages
                                          .ToList();
                 }
             }
+        }
+
+        private async void UserProductsListView_ProductClick(object sender, ItemClickEventArgs e)
+        {
+            var clickedItem = (Product)e.ClickedItem;
+
+            ContentDialog itemDetailsDialog = new ContentDialog()
+            {
+                Title = clickedItem.Name,
+                Content = $"{clickedItem.Description}\n" +
+                          $"Price: ${clickedItem.Price}\n",
+                CloseButtonText = "OK",
+                XamlRoot = this.XamlRoot
+            };
+            await itemDetailsDialog.ShowAsync();
         }
 
         private void LogoutButton_Click(object sender, RoutedEventArgs e)
